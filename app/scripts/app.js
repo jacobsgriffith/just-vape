002244f759ae4266ea637e03689a8590bee55494
Jacob Griffith <jgriffith@fadboard.com>
2015-05-20 13:32:31 -0600

    added angular

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name fadboardAppApp
+ * @description
+ * # fadboardAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('fadboardAppApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
