95c8a50fb3aac65af202aaee670d9df88612a020
Jacob Griffith <jgriffith@fadboard.com>
2015-05-20 20:57:12 -0600

    hopefully completed the deviceready code

--- emulate.bat
+++
@@ -1,9 +0,0 @@
-call grunt build
-copy config.xml dist
-call grunt compress
-mkdir bin
-call grunt phonegap-build
-
-cmd /K "C:\Program Files (x86)\Microsoft SDKs\Windows Phone\v8.1\Tools\AppDeploy\AppDeployCmd.exe" /installlaunch %~dp0/bin/windowsphone.xap /targetdevice:xd
-
-exit
