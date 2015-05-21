875165220b2b5bddd11cdbe90eed647e6999b0f2
Jacob Griffith <jgriffith@fadboard.com>
2015-05-20 23:07:35 -0600

    Login modal dialog and authentication on fadboard.com

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,27 +9,107 @@
  * Main module of the application.
  */
 angular
-  .module('fadboardAppApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider, $compileProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-	  $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|ghttps?|ms-appx|x-wmapp0):/);
-	  //$compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
-  });
+	.module('fadboardAppApp', [
+	'ngAnimate',
+	'ngCookies',
+	'ngResource',
+	'ui.bootstrap',
+	'ui.router',
+	'ngSanitize',
+	'ngTouch'
+	]).config(function (/*$routeProvider, */$compileProvider, $stateProvider, $httpProvider) {
+	
+		$stateProvider
+		.state('home', {
+			url: "/",
+			templateUrl: "views/main.html",
+			controller: 'MainCtrl',
+			data: {
+				requireLogin: false
+			}
+		})
+		.state('about', {
+			url: "/about",
+			templateUrl: "views/about.html",
+			controller: 'AboutCtrl',
+			data: {
+				requireLogin: true
+			}
+		});
+	
+	
+		/*$routeProvider.when('/', {
+			templateUrl: 'views/main.html',
+			controller: 'MainCtrl'
+		}).when('/about', {
+			templateUrl: 'views/about.html',
+			controller: 'AboutCtrl'
+		}).otherwise({
+			redirectTo: '/'
+		});*/
+
+		$compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|ghttps?|ms-appx|x-wmapp0):/);
+
+		/*$stateProvider.state('welcome', {
+			url: '/welcome',
+			data: {
+				requireLogin: false
+			}
+		}).state('app', {
+			abstract: true,
+			data: {
+				requireLogin: true // this property will apply to all children of 'app'
+			}
+		})
+		.state('app.dashboard', {
+		// child state of `app`
+		// requireLogin === true
+		})*/
+		
+		
+		$httpProvider.interceptors.push(function ($timeout, $q, $injector) {
+		var loginModal, $http, $state;
+
+		// this trick must be done so that we don't receive
+		// `Uncaught Error: [$injector:cdep] Circular dependency found`
+		$timeout(function () {
+		  loginModal = $injector.get('loginModal');
+		  $http = $injector.get('$http');
+		  $state = $injector.get('$state');
+		});
+
+		return {
+		  responseError: function (rejection) {
+			if (rejection.status !== 401) {
+			  return rejection;
+			}
+
+			var deferred = $q.defer();
+
+			loginModal()
+			  .then(function () {
+				deferred.resolve( $http(rejection.config) );
+			  })
+			  .catch(function () {
+				$state.go('home');
+				deferred.reject(rejection);
+			  });
+
+			return deferred.promise;
+		  }
+		};
+	  });
+		
+	}).run(function ($rootScope, $state, loginModal) {
+		$rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
+		var requireLogin = toState.data.requireLogin;
+		if (requireLogin && typeof $rootScope.currentUser === 'undefined') {
+				event.preventDefault();
+				loginModal().then(function () {
+					return $state.go(toState.name, toParams);
+				}).catch(function () {
+					return $state.go('home');
+				});
+			}
+		});
+	});
---
+++ app/scripts/services/user.js
@@ -0,0 +1,34 @@
+'use strict';
+/*angular.module('fadboardAppApp')
+  .service('CordovaReady', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+});*/
+
+angular.module('fadboardAppApp').factory("UsersApi", ['$q', '$http', function($q, $http) {
+	function _login(email, password) {
+		var deferred = $q.defer();
+		$http({
+			method: 'POST',
+			url: 'http://fadboard.com/cmsadmin/logintest.php',
+			headers: {
+				'Content-type': 'application/x-www-form-urlencoded',
+				'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
+			},
+			data: 'login_util=' + email + '&pass_util=' + password
+			//responseType: 'arraybuffer'
+		}).success(function(data, status, headers, config) {
+		//$http.post('http://fadboard.com/cmsadmin/logintest.php', 'login_util=' + email + '&pass_util=' + password).success(function(data, status, headers, config) {
+			if (data.indexOf('true') != -1) {
+				deferred.resolve({name: 'Admin'});
+			} else {
+				deferred.reject();
+			}
+		}).error(function(data, status, headers, config) {
+			deferred.reject();
+		});
+		return deferred.promise
+	}
+	return {
+		login: _login
+	};
+}]);
